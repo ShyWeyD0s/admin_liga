45ee4b591c05d68f95fb8ecfedaf6fcbaeddb260
ShyWeyD0s <sebastianchandesu62@gmail.com>
2026-05-05 14:54:39 -0500

    add: consultas en controller creadas

--- Admin_Liga/app/Http/Controllers/GameController.php
+++ Admin_Liga/app/Http/Controllers/GameController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Game;
 
 class GameController extends Controller
 {
-    //
+    //Consulta Eloquent todos los partidos en la ruta /games
+    public function index()
+    {
+        $games = Game::all();
+        return $games;     
+    }
+    
+        
+   //Consulta Eloquent todos los partidos con sus equipos y la tabla intermedia
+    public function index2()
+    {
+        $games2 = Game::with('teams', 'teamGames')->get();
+        return $games2;
+    }
 }
