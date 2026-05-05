c97a891e0c54d223dcba6e885181db331f188725
ShyWeyD0s <sebastianchandesu62@gmail.com>
2026-05-05 15:05:15 -0500

    fix: consultas arregladas

--- Admin_Liga/app/Http/Controllers/PlayerController.php
+++ Admin_Liga/app/Http/Controllers/PlayerController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Player;
 
 class PlayerController extends Controller
 {
     //
+
+    public function index()
+    {
+        $players = Player::all();
+        return $players;
+    }
+
+    public function index2()
+    {
+        $players2 = Player::with('team', 'goals')->get();
+        return $players2;
+    }
 }
--- Admin_Liga/routes/web.php
+++ Admin_Liga/routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TeamController;
+use App\Http\Controllers\PresidentController;
+use App\Http\Controllers\PlayerController;
+use App\Http\Controllers\GameController;
+use App\Http\Controllers\TeamGameController;
+use App\Http\Controllers\GoalController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +22,43 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/teams', 
+    [TeamController::class, 'index']
+);
+Route::get('/teams2', 
+    [TeamController::class, 'index2']
+);
+Route::get('/presidents', 
+    [PresidentController::class, 'index']
+);
+Route::get('/presidents2', 
+    [PresidentController::class, 'index2']
+);
+Route::get('/players', 
+    [PlayerController::class, 'index']
+);
+Route::get('/players2', 
+    [PlayerController::class, 'index2']
+);
+Route::get('/games', 
+    [GameController::class, 'index']
+);
+Route::get('/games2', 
+    [GameController::class, 'index2']
+);
+Route::get('/teamGames', 
+    [TeamGameController::class, 'index']
+);
+Route::get('/teamGames2', 
+    [TeamGameController::class, 'index2']
+);
+Route::get('/goals', 
+    [GoalController::class, 'index']
+);
+Route::get('/goals2', 
+    [GoalController::class, 'index2']
+);
 
 
 
